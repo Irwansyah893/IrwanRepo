0d10a55dd8b240972a3ffc34856559846ba37889
Mhd_Irwansyah <140039137+Irwansyah893@users.noreply.github.com>
2024-03-06 15:11:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Biodata Irwansyah</title>
+  </head>
+  <body>
+    <?php 
+      echo "Nama : Muhammad Irwansyah";
+      echo "Kelas : 22Mi-A";
+      echo "Alamat : Merpati Sakti";
+    ?>
+  </body>
+</html>
